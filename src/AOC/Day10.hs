cdb2908f3c50661ed83198662e512dfce2cf1f87
Emily Martins <emily.flakeheart@gmail.com>
2020-12-10 15:16:02 +0100

    day 10 implementation

--- src/AOC/Day10.hs
+++ src/AOC/Day10.hs
@@ -4,17 +4,60 @@ import AOC.Common.Prelude
 
 import Data.List
 import Data.Maybe
+import qualified Data.Array as Array
+import Data.Array (Array, (!))
+import qualified Data.Map as Map
+import Data.Map (Map)
+import Debug.Trace
 
 type Input = [Int]
 
 parser :: Parser Input
 parser = many integer
 
+next :: Array Int Int -> Map Int [Int]
+next xs =
+  let (low, high) =
+        Array.bounds xs
+  in
+    Map.fromList
+  $ fmap
+  (\i ->
+      ( i
+      , [ i + o
+        | o <- [1..3]
+        , i + o <= high
+        , let (a, b) = (xs ! i, xs ! (i + o))
+        , b > a && b <= a + 3
+        ] )
+  )
+  [low..high]
+
 one :: Input -> _
-one xs = sum xs
+one xs =
+  let
+    joltPath = sort xs
+    differences = fmap abs $ zipWith (-) joltPath (tail joltPath)
+  in
+    ((length . filter (==3) $ differences) + 1)
+  * ((length . filter (==1) $ differences) + 1)
 
 two :: Input -> _
-two xs = product xs
+two xs =
+  let
+    ordering = sort xs
+    arr = Array.listArray (0, length ordering - 1) ordering
+    optionsFrom =
+      Map.mapWithKey
+      (\k jmps ->
+         case jmps of
+           [] -> 1
+           _ -> sum $ fmap (optionsFrom Map.!) jmps
+      )
+      (next arr)
+  in
+  sum $ (optionsFrom Map.!) <$> [0..2]
+
 
 tests :: SpecWith ()
 tests = pure ()
